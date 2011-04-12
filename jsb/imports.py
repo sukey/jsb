9fe054da6cc1740850ef45e79c8d5bf548f775b2
Bart Thate <bthate@gmail.com>
2011-04-12 15:05:05 +0200

    add requests to the contrib

--- jsb/imports.py
+++ jsb/imports.py
@@ -21,11 +21,16 @@ def getjson():
     except ImportError:
         try: mod = _import("json")
         except:
+<<<<<<< local
+            mod = _import("jsb.contrib.simplejson")
+    logging.info("imports - json module is %s" % str(mod))
+=======
             try:
                 mod = _import("simplejson")
             except:
                 mod = _import("jsb.contrib.simplejson")
     logging.debug("imports - module is %s" % str(mod))
+>>>>>>> other
     return mod
 
 ## getfeedparser function
@@ -34,12 +39,19 @@ def getfeedparser():
     try: mod = _import("feedparser")
     except:
         mod = _import("jsb.contrib.feedparser")
-    logging.debug("imports - module is %s" % str(mod))
+    logging.info("imports - feedparser module is %s" % str(mod))
     return mod
 
 def getoauth():
     try: mod = _import("oauth")
     except:
         mod = _import("jsb.contrib.oauth")
-    logging.debug("imports - module is %s" % str(mod))
+    logging.info("imports - oauth module is %s" % str(mod))
+    return mod
+
+def getrequests():
+    try: mod = _import("requests")
+    except:
+        mod = _import("jsb.contrib.requests")
+    logging.info("imports - requests module is %s" % str(mod))
     return mod
